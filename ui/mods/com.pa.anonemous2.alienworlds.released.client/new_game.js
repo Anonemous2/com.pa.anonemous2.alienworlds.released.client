3c5883d8f9a5310ded52e925ba74ebecc0695df9
Anonemous2 <35852034+Anonemous2@users.noreply.github.com>
2019-01-04 22:54:35 -0500

    2.2

---
+++ ui/mods/com.pa.anonemous2.alienworlds.released.client/new_game.js
@@ -0,0 +1,7 @@
+model.biomes.push('alienred'),
+model.biomes.push('alienpurple'),
+model.biomes.push('alientoxic'),
+model.biomes.push('SuperJovian'),
+model.biomes.push('SuperJovianTerrain'),
+model.biomes.push('Sun'),
+model.biomes.push('SunTerrain')
