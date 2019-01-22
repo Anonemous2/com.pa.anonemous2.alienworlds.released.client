b0bfd01662c8f6f1825bc6aa346e426f82f85bb5
Anonemous2 <35852034+Anonemous2@users.noreply.github.com>
2019-01-21 19:50:32 -0500

    fixed for linux

--- ui/mods/com.pa.anonemous2.alienworlds.released.client/new_game.js
+++ ui/mods/com.pa.anonemous2.alienworlds.released.client/new_game.js
@@ -1,7 +1,7 @@
 model.biomes.push('alienred'),
 model.biomes.push('alienpurple'),
 model.biomes.push('alientoxic'),
-model.biomes.push('SuperJovian'),
-model.biomes.push('SuperJovianTerrain'),
-model.biomes.push('Sun'),
-model.biomes.push('SunTerrain')
+model.biomes.push('superJovian'),
+model.biomes.push('superJovianTerrain'),
+model.biomes.push('sun'),
+model.biomes.push('sunTerrain')
